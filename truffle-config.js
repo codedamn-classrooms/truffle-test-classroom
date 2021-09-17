1ea67c50a86095442a466fe972ea2ed23abc1c2c
Mehul Mohan <mehulmpt@gmail.com>
2021-09-18 04:36:18 +0530

    Truffle base config

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	networks: {},
+
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		useColors: false,
+		reporter: 'mochawesome',
+		reporterOptions: {
+			reportFilename: process.env.UNIT_TEST_OUTPUT_FILE + '.json',
+			json: true,
+			html: false,
+		},
+	},
+
+	// Configure your compilers
+	compilers: {},
+}
